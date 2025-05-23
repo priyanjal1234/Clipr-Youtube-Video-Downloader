af202f3e821ec516207d0bb46053eb11a1289704
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-05-23 11:42:03 +0530

    Created the controller for fetching the video information

--- apps/server/src/controllers/youtube.controller.ts
+++ apps/server/src/controllers/youtube.controller.ts
@@ -1,9 +1,64 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 
-export const getVideoInfo = function(req: Request,res: Response) {
-    try {
-        
-    } catch (error) {
-        
+import ytdlp from "yt-dlp-exec";
+
+import youtubeDl from "youtube-dl-exec";
+
+export const getVideoInfo = async function (req: Request, res: Response) {
+  try {
+    let { videoUrl } = req.body;
+
+    let info = await ytdlp(videoUrl, {
+      dumpSingleJson: true,
+      noCheckCertificate: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+    });
+
+    let videoDetails = {
+      title: info.title,
+      thumbnail: info.thumbnail,
+      duration: info.duration,
+      channel: info.uploader,
+      views: info.view_count,
+    };
+
+    return res.status(200).json(videoDetails);
+  } catch (error) {
+    return res.status(500).json({
+      message: error instanceof Error ? error.message : "Error fetching info",
+    });
+  }
+};
+
+export const downloadVideo = function (req: Request, res: Response) {
+  try {
+    let { videoUrl } = req.body;
+
+    if (!videoUrl) {
+      return res.status(400).json({ message: "Video Url is required" });
     }
-}
+
+    res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
+    res.setHeader("Content-Type", "video/mp4");
+
+    youtubeDl(videoUrl, {
+      dumpSingleJson: true,
+      noCheckCertificates: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+      
+    })
+      .then((output) => {
+        console.log("Video Info:", output);
+        res.json(output);
+      })
+      .catch((err) => {
+        console.error("yt-dlp error", err);
+        res.status(500).json({ message: "Failed to fetch video info" });
+      });
+
+    res.json({});
+  } catch (error) {}
+};
--- apps/server/src/routes/youtube.router.ts
+++ apps/server/src/routes/youtube.router.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import { getVideoInfo } from "../controllers/youtube.controller";
+import { downloadVideo, getVideoInfo } from "../controllers/youtube.controller";
 import urlValidation from "../middlewares/urlValidation";
 
 const router = express.Router();
@@ -8,6 +8,14 @@ router
   .route("/video/info")
   .post((req: Request, res: Response, next: NextFunction) => {
     urlValidation(req, res, next);
-  }, getVideoInfo);
+  }, (req: Request,res: Response) => {
+    getVideoInfo(req,res)
+  });
+
+router.route("/video/download").post((req: Request,res: Response, next: NextFunction) => {
+    urlValidation(req,res,next)
+},(req: Request,res: Response) => {
+    downloadVideo(req,res)
+})
 
 export default router;
